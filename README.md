de8abb3dbcd6886bfe7175c5c3c86e53e34765be
abigailweber <37055142+abigailweber@users.noreply.github.com>
2019-09-25 19:50:10 -0400

    Lab 1 Complete

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Template for course repository
 
 ## VR Lab: 1
-In Lab 1 I intend to have players place chickens and cows in their respective pens.
+In Lab 1 I intend to have players pick up and place chickens and pigs and place them into their marked pens. Upon completion players will be given a "win" text.
+Assets outside of the farm scene assets used were: https://assetstore.unity.com/packages/3d/farm-animals-set-97945 
+
+
+rumpelstiltskin
---
+++ in-class/wk1/Assets/LevelManagementScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public bool chickenOne = false;
+    public bool chickenTwo = false;
+    public bool chickenThree = false;
+    public bool chickenFour = false;
+    public bool chickenFive = false;
+    public bool pigOne = false;
+    public bool pigTwo = false;
+    public bool pigThree = false;
+    public bool pigFour = false;
+    public bool pigFive = false;
+
+    public GameObject finishText;
+    public GameObject startText;
+
+
+    public void checkForVictory()
+    {
+        if (chickenOne && chickenTwo && chickenThree && chickenFour && chickenFive && pigOne && pigTwo && pigThree && pigFour && pigFive)
+        {
+            finishText.SetActive(true);
+            startText.SetActive(false);
+        }
+
+    }
+
+    public void chickenOneIn()
+    {
+        this.chickenOne = true;
+        checkForVictory();
+    }
+    public void chickenTwoIn()
+    {
+        this.chickenTwo = true;
+        checkForVictory();
+    }
+    public void chickenThreeIn()
+    {
+        this.chickenThree = true;
+        checkForVictory();
+    }
+    public void chickenFourIn()
+    {
+        this.chickenFour = true;
+        checkForVictory();
+    }
+    public void chickenFiveIn()
+    {
+        this.chickenFive = true;
+        checkForVictory();
+    }
+    public void pigOneIn()
+    {
+        this.pigOne = true;
+        checkForVictory();
+    }
+    public void pigTwoIn()
+    {
+        this.pigTwo = true;
+        checkForVictory();
+    }
+    public void pigThreeIn()
+    {
+        this.pigThree = true;
+        checkForVictory();
+    }
+    public void pigFourIn()
+    {
+        this.pigFour = true;
+        checkForVictory();
+    }
+    public void pigFiveIn()
+    {
+        this.pigFive = true;
+        checkForVictory();
+    }
+
+}
+
+
